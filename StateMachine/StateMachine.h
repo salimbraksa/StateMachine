12f30de767778bc62914d6044b5db637c3d5b7aa
Salim <salim.braksa@gmail.com>
2016-07-17 15:16:03 +0100

    Initial Commit

---
+++ StateMachine/StateMachine.h
@@ -0,0 +1,19 @@
+//
+//  StateMachine.h
+//  StateMachine
+//
+//  Created by Salim Braksa on 7/17/16.
+//  Copyright © 2016 Salim Braksa. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for StateMachine.
+FOUNDATION_EXPORT double StateMachineVersionNumber;
+
+//! Project version string for StateMachine.
+FOUNDATION_EXPORT const unsigned char StateMachineVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <StateMachine/PublicHeader.h>
+
+
